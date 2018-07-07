e255492035b425b694a9ec8d81415d9e375dcd25
Sean Newman <seangnewman@msn.com>
2018-07-06 20:27:25 -0700

    update day one

--- HackerRank/DayZero/Java/HelloWorld.java
+++ HackerRank/DayZero/Java/HelloWorld.java
@@ -21,6 +21,6 @@ public class Solution {
 		System.out.println("Hello, World.");
       
 	    // TODO: Write a line of code here that prints the contents of inputString to stdout.
-    System.out.println (inputString);
+    	System.out.println (inputString);
 	}
 }
